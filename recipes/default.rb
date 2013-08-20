5419e5051beb2328278307ae89624e667bca2237
Wil Reichert <wil.reichert@kt.com>
2013-08-20 23:44:37 +0900

    etcd updates

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,16 +17,20 @@ register_member("image-api", d1)
 d2 = get_openstack_service_template(get_interface_address("management"), "9191")
 register_member("image-registry", d2)
 
+# dependant services
 set_rabbit_servers "image-api"
 set_database_servers "image"
-set_service_endpoint_ip "image-api"
-set_service_endpoint_ip "image-registry"
+set_service_endpoint "identity-api"
+set_service_endpoint "identity-admin"
+set_service_endpoint "image-registry"
+set_service_endpoint "image-api"
+
 node.default["openstack"]["image"]["api"]["bind_interface"] = get_interface "management"
 node.default["openstack"]["image"]["registry"]["bind_interface"] = get_interface "management"
 
 include_recipe "openstack-common"
 include_recipe "openstack-common::logging"
-include_recipe "openstack-image::api"
 include_recipe "openstack-image::registry"
+include_recipe "openstack-image::api"
 include_recipe "openstack-image::identity_registration"
 
