1e8ec171058fc834adf21883aa79d74954c7360f
ktc-jenkins <jenkins.ktcloudware@gmail.com>
2013-11-19 15:45:25 +0900

    auto_version to 0.3.5

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'wil.reichert@kt.com'
 license          'All rights reserved'
 description      'Installs/Configures ktc-image'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.3.4'
+version '0.3.5'
 
 %w{ centos ubuntu }.each do |os|
   supports os
